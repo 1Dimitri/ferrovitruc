93fe67122a3f318f1e2671f8cfb179424f1b1eb1
1Dimitri <git.dimitri@janczak.net>
2019-10-17 18:11:37 +0200

    refactoring 3 using ordered hash table

--- lexique-et-tr3/CreateTR3andGlossary.ps1
+++ lexique-et-tr3/CreateTR3andGlossary.ps1
@@ -23,50 +23,61 @@ function Split-TR3andGlossary {
     $csv = Import-CSV -Path $tempfilename -Encoding UTF8 -Delimiter ';'
     
     $TextInfo = (Get-Culture).TextInfo
-    $TR3List = [System.collections.ArrayList]@()
-    $LexiconList = [System.collections.ArrayList]@()
+    $TR3List = [ordered]@{ }
+    $LexiconList = [ordered]@{ }
     $csv | ForEach-Object { 
         # Définition and Abréviation titles are not properly handled by $_."Définition" syntax due to editor codepages etc
         # workaround get the properties by index, skip 1st field, get 2 and 3
         $PropsNValues = $_.PSObject.Properties | Select-Object -Skip 1 -First 2
         $term = $PropsNValues[0].Value
 
-        if (($term.Length -le 3) -and ($PropsNValues[1].Value -match "(.+) \(gare\)")) {
-            $station= $matches[1]
-            Write-Verbose "Found train station: $station"
-            $TR3List.Add([PSCustomObject]@{
-                # yet another workaround: TitleCase only works with starting lower case string;
-                station = $TextInfo.ToTitleCase($station.ToLower())
-                tr3 = $term
-            }) | Out-Null
-        } else { # not a train station
+        if (($term.Length -le 3) -and ($PropsNValues[1].Value -match "(.+) \(gare\)") -and (!$term.StartsWith('VISITE'))) {
+            # TO DO: CHANGE ALGORITHM..
+            #VTE corresponds both to VITRE (GARE) and VISITE TECHNIQUE D'ECHANGE (GARE)
+            $station = $matches[1]
             
-            $meaning= $PropsNValues[1].Value -replace '_x000D_','. '
-            Write-Verbose "Found glossary entry: $term"
-            $LexiconList.Add([PSCustomObject]@{
-                # yet another workaround: TitleCase only works with starting lower case string;
-                term = $term
-                meaning = $meaning               
-            }) | Out-Null
+            Write-Verbose "Found train station: $station"
+            $TR3List.Add($term, $TextInfo.ToTitleCase($station.ToLower()))
+            # yet another workaround: TitleCase only works with starting lower case string;           
         }
+        else {
+            # not a train station
+            
+            $meaning = $PropsNValues[1].Value -replace '_x000D_', '. '
+            Write-Verbose "Found glossary entry: $term"
+            if ($LexiconList.Contains($term)) {
+                $add_meaning = $LexiconList[$term]
+                $LexiconList[$term]= $meaning + $add_meaning
+            }
+            else {
+                $LexiconList.Add($term, $meaning)
+            }
+        
+    
+        }
+
     }
 
     Remove-Item $tempfilename -Force
-    ($TR3List,$LexiconList)
+    ($TR3List, $LexiconList)
 }
+
 
 # Create markdown .md files tr3.Md and gare.md from previous function
 function New-TR3GlossaryMdFiles {
 
-    ($tr3,$lex) = Split-TR3andGlossary
+    ($tr3, $lex) = Split-TR3andGlossary
 
     $mdFiles = @(
-        @{title = 'Lexique SNCF';fname='Lexique-SNCF.md';col1='Abréviation';col2='signification';
-        linesb= { @("# $($_.term)","$($_.meaning)")};list=$lex},
-        @{title = 'TR3 -> Gare SNCF';fname='TR3.md';col1='Abréviation';col2='Nom de la gare';
-        linesb={"| $($_.tr3) | $($_.station) |"};list=$tr3},
-        @{title = 'Gare -> TR3';fname='gare.md';col1='Nom de la gare';col2='Abréviation';
-        linesb={"| $($_.station) | $($_.tr3) |"};list=$tr3}
+        @{title = 'Lexique SNCF'; fname = 'Lexique-SNCF.md'; col1 = 'Abréviation'; col2 = 'signification';
+            linesb = { @("# $($_.key)", "$($_.value)") }; list = $lex
+        },
+        @{title = 'TR3 -> Gare SNCF'; fname = 'TR3.md'; col1 = 'Abréviation'; col2 = 'Nom de la gare';
+            linesb = { "| $($_.key) | $($_.value) |" }; list = $tr3
+        },
+        @{title = 'Gare -> TR3'; fname = 'gare.md'; col1 = 'Nom de la gare'; col2 = 'Abréviation';
+            linesb = { "| $($_.value) | $($_.key) |" }; list = $tr3
+        }
     )
         
       
@@ -81,7 +92,7 @@ function New-TR3GlossaryMdFiles {
 
         # todo: put contents
         $sb = $_.linesb
-        $_.list | ForEach-Object {
+        $_.list.GetEnumerator() | ForEach-Object {
             &$sb | Add-Content $fn -Force
         }
         "" | Add-Content $fn -Force
